d697b43c575545f9800473f2818887fb21bbffbb
Rao <Rao@reeko.net.cn>
2024-11-29 14:42:56 +0800

    修改测试位置

--- server/test.js
+++ server/test.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 // 测试抽奖结果数据的正确性
 var selected = {},
   repeat = [],
-  luckyData = require("/Users/xiechang/Documents/project/抽奖/product/dist/temp.json"),
-  errorData = require("/Users/xiechang/Documents/project/抽奖/product/dist/error.json");
+  luckyData = require("./cache/temp.json"),
+  errorData = require("./cache/error.json");
 
 for (let key in luckyData) {
   let item = luckyData[key];
